c416b49fdb2d73e60c77d6b4185d7ed986f7ca5b
Michael Edie <34865864+c0demech@users.noreply.github.com>
2018-01-13 11:15:42 -0500

    Update loop.c

--- RevEng/loop.c
+++ RevEng/loop.c
@@ -9,7 +9,6 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * Demonstrate variable declarations
  ***************************************************************************/
 
 int
